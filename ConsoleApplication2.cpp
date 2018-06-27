421c8c1467c5ef222914918f521edd39f9c5839d
testtesttesttesttesttesttes <40618200+testtesttesttesttesttesttes@users.noreply.github.com>
2018-06-27 12:29:17 +1000

    Calculate divided

--- ConsoleApplication2.cpp
+++ ConsoleApplication2.cpp
@@ -1,4 +1,4 @@
-// ConsoleApplication2.cpp: определяет точку входа для консольного приложения.
+// ConsoleApplication1.cpp: определяет точку входа для консольного приложения.
 //
 
 #include "stdafx.h"
@@ -7,20 +7,31 @@
 int main()
 {
 	using namespace std;
-
+	
 	double x, y, res; //
 	char z;//Объявляем переменный.
 start:
 	cin >> x;//
-	cin >> z;//Присваеваем значение переменным.
-	cin >> y;//
+cin >> z;//Присваеваем значение переменным.
+cin >> y;//
 	if (z == '+')
 		res = x + y;
 	else if (z == '-')
 		res = x - y;
 	else if (z == '*')
 		res = x*y;
-	cout << "result: " << res << endl;//Выводим на экран результат, std::endl-переход на следующею строку.
+	else if (z == '/')
+	{
+		if (y == 0)//Если "Y" равен нулю то выводим сообщение 
+		{
+			cout << "can not be divided by zero\n";
+			goto start;//Возврат в начало программы
+		}
+		else if (y != 0)//Если "Y" не равен нулю то производим деление
+			res = x / y;
+	}
+
+     cout << "result: " << res << endl;//Выводим на экран результат, std::endl-переход на следующею строку.
 
 	goto start;//Возврат в начало программы
 	return(0);
